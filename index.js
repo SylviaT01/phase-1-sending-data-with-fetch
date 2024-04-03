64f996fa78e11418e9ca3e90b232e033f4a788d9
Sylvia <sylviachebet03@gmail.com>
2024-04-03 17:20:55 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: "Steve",
+        email: "steve@steve.com"
+    };
+    const config = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    return fetch('http://localhost:3000/users', config)
+        .then(response => response.json())
+        .then(data => document.body.innerHTML = data.id)
+        .catch(error => document.body.innerHTML = error.message)
+}
+
+
+
